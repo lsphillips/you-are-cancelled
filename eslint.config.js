0809d41ea86fa6d6debd2969ec780db347d68d54
lsphillips <lsphillips@users.noreply.github.com>
2025-03-28 11:50:28 +0000

    Updated the ESLint setup.

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import protectMeFromMyStupidity from 'eslint-config-protect-me-from-my-stupidity';
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default [
+	{
+		ignores : [
+			'you-are-cancelled.js',
+			'you-are-cancelled.cjs',
+			'you-are-cancelled.es5.js',
+			'you-are-cancelled.es5.cjs'
+		]
+	},
+	{
+		languageOptions :
+		{
+			globals : {
+				'AbortController' : 'writable',
+				'AbortSignal'     : 'writable'
+			}
+		}
+	},
+	...protectMeFromMyStupidity()
+];
